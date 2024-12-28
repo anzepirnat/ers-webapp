bf5916eadf43a8dde36f68c7c40be6857715c543
anzepirnat <anze.pirnat2001@gmail.com>
2024-12-28 19:52:24 +0100

    Added functionality to admin for progress tracking

--- ers_evaluation/admin.py
+++ ers_evaluation/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-
 from .models import Evaluation, Recommendation
 
-#admin.site.register(Evaluation)
+MAX_EVALUATIONS = 3
+
+@admin.register(Evaluation)
+class EvaluationAdmin(admin.ModelAdmin):
+    list_display = ("rating", "comment", "user_id")
+    list_filter = ("user_id",)
+    change_list_template = "admin/evaluation_changelist.html"
+
+    def changelist_view(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['max_evaluations'] = MAX_EVALUATIONS
+        return super(EvaluationAdmin, self).changelist_view(request, extra_context=extra_context)
+
 admin.site.register(Recommendation)
